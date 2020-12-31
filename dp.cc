087b259822aa7daa4cc47b1c7dc2f749271d04e6
Tanishq Kumar <thetanishq@gmail.com>
2021-01-01 00:23:06 +0400

    bs push

---
+++ dp.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "helpers.hh"
+#include <algorithm>
+
+// rod-cutting
+
+// chain matrix multiplication
+
+// longest common subsequence
+
+// travelling salesman
+
+int main(){
+    
+}
